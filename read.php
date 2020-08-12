f1378a2b3973d17827ce637ec63ac401aefd8294
ullaskaluvala <57393931+ullaskaluvala@users.noreply.github.com>
2020-08-12 19:33:45 +0530

    Add files via upload
    Complete project code for test case

---
+++ read.php
@@ -0,0 +1,16 @@
+<?php
+include 'functions.php';
+// Connect to MySQL database
+$pdo = pdo_connect_mysql();
+// Get the page via GET request (URL param: page), if non exists default the page to 1
+$page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
+// Number of records to show on each page
+$records_per_page = 5;
+$stmt = $pdo->prepare('SELECT * FROM contacts ORDER BY id LIMIT :current_page, :record_per_page');
+$stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
+$stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
+$stmt->execute();
+// Fetch the records so we can display them in our template.
+$contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$num_contacts = $pdo->query('SELECT COUNT(*) FROM contacts')->fetchColumn();
+?>
